4adbdf7db57934a67e2cf397f5a4d21868a0773a
Andrii B <andrii.b.as@agiliumlabs.com>
2021-11-24 17:06:55 +0000

    added d.ts files

--- lib/clients/errorHandler.d.ts
+++ lib/clients/errorHandler.d.ts
@@ -1,5 +1,3 @@
-'use strict';
-
 /**
  * Base class for API errors. Contains indication of HTTP status.
  */
@@ -9,55 +7,33 @@ export class ApiError extends Error {
    * ApiError constructor
    * @param {Function} clazz error name
    * @param {Object} message error message
-   * @param {number} status HTTP status
+   * @param {Number} status HTTP status
    */
-  constructor(clazz, message, status) {
-    super(message);
-    /**
-     * Error name
-     * @type {string}
-     */
-    this.name = clazz.name;
-    /**
-     * HTTP status code
-     * @type {number}
-     */
-    this.status = status;
-    Error.captureStackTrace(this, clazz);
-  }
+  constructor(clazz: Function, message: Object, status: Number);
 
   /**
    * Sets error code, used for i18n
-   * @param {string} code error code for i18n
+   * @param {String} code error code for i18n
    */
-  set code(code) {
-    this._code = code;
-  }
+  set code(code: String);
 
   /**
    * Returns error code used for i18n
-   * @return {string} error code
+   * @return {String} error code
    */
-  get code() {
-    return this._code;
-  }
+  get code(): String;
 
   /**
    * Set message arguments for i18n
    * @param {Array<Object>} args arguments for i18n
    */
-  set arguments(args) {
-    this._args = args;
-  }
+  set arguments(args: Array<Object>);
 
   /**
    * Returns message arguments for i18n
    * @return {Array<Object>} message arguments for i18n
    */
-  get arguments() {
-    return this._args;
-  }
-
+  get arguments(): Array<Object>;
 }
 
 /**
@@ -67,12 +43,9 @@ export class NotFoundError extends ApiError {
 
   /**
    * Represents NotFoundError.
-   * @param {string} message error message
+   * @param {String} message error message
    */
-  constructor(message) {
-    super(NotFoundError, message, 404);
-  }
-
+  constructor(message: String);
 }
 
 /**
@@ -82,12 +55,9 @@ export class ForbiddenError extends ApiError {
 
   /**
    * Constructs forbidden error.
-   * @param {string} message error message
+   * @param {String} message error message
    */
-  constructor(message) {
-    super(ForbiddenError, message, 403);
-  }
-
+   constructor(message: String);
 }
 
 /**
@@ -97,12 +67,9 @@ export class UnauthorizedError extends ApiError {
 
   /**
    * Constructs unauthorized error.
-   * @param {string} message error message
+   * @param {String} message error message
    */
-  constructor(message) {
-    super(UnauthorizedError, message, 401);
-  }
-
+   constructor(message: String);
 }
 
 /**
@@ -112,53 +79,59 @@ export class ValidationError extends ApiError {
 
   /**
    * Constructs validation error.
-   * @param {string} message error message
+   * @param {String} message error message
    * @param {Object} details error data
    */
-  constructor(message, details) {
-    super(ValidationError, message, 400);
-    /**
-     * Validation error details
-     * @type {Object}
-     */
-    this.details = details;
-  }
-
+  constructor(message: String, details: Object);
 }
 
 /**
  * Represents unexpected error. Throwing this error results in 500 (Internal Error) HTTP response code.
  */
 export class InternalError extends ApiError {
-
+  
   /**
    * Constructs unexpected error.
-   * @param {string} message error message
+   * @param {String} message error message
    */
-  constructor(message) {
-    super(InternalError, message, 500);
-  }
-
+  constructor(message: String);
 }
 
 /**
- * @typedef {Object} TooManyRequestsErrorMetadata metadata for too many requests error
- * @property {Number} periodInMinutes throttling period in minutes
- * @property {Number} requestsPerPeriodAllowed available requests for periodInMinutes
- * @property {Date} recommendedRetryTime recommended date to retry request
+ * metadata for too many requests error
  */
+export type TooManyRequestsErrorMetadata = {
+
+  /**
+   * periodInMinutes throttling period in minutes
+   */
+  periodInMinutes: Number,
+
+  /**
+   *requestsPerPeriodAllowed available requests for periodInMinutes
+   */
+  requestsPerPeriodAllowed: Number,
+
+  /**
+   * recommendedRetryTime recommended date to retry request
+   */
+  recommendedRetryTime: Date,
+
+  /**
+   * type error type
+   */
+  type: String
+}
 
 /**
  * Represents too many requests error. Throwing this error results in 429 (Too Many Requests) HTTP response code.
  */
 export class TooManyRequestsError extends ApiError {
+
   /**
    * Constructs too many requests error.
-   * @param {string} message error message
+   * @param {String} message error message
    * @param {TooManyRequestsErrorMetadata} metadata error metadata
    */
-  constructor(message, metadata) {
-    super(TooManyRequestsError, message, 429);
-    this.metadata = metadata;
-  }
+  constructor(message: String, metadata: TooManyRequestsErrorMetadata);
 }
